7b4b8ff86b3049c43cc760dd461b81f254f87805
ChenYu Geng <92907282+ChenYu-G@users.noreply.github.com>
2023-05-04 20:49:21 +0000

    connect db

--- server/index.js
+++ server/index.js
@@ -2,18 +2,25 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 
+import connectDB from './mongodb/connect'
+
 dotenv.config()
 
 const app = express()
 app.use(cors())
-app.use(express.json({limit: '50mb'}))
+app.use(express.json({ limit: '50mb' }))
 
-app.get('/', async(req,res) => {
-    res.send('Hello from DALL-E')
+app.get('/', async (req, res) => {
+  res.send('Hello from DALL-E!')
 })
 
-const startServer = async () => { 
-    app.listen(8000, () => console.log('Server started on port 8080'));
- }
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL)
+    app.listen(8000, () => console.log('Server started on port 8080'))
+  } catch (error) {
+    console.log(error)
+  }
+}
 
- startServer()
+startServer()
