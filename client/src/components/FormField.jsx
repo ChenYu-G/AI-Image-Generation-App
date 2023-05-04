6c93d6b25dfe0691578714765ae3c993a6003595
ChenYu Geng <92907282+ChenYu-G@users.noreply.github.com>
2023-05-04 20:21:21 +0000

    update

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -19,7 +19,29 @@ const FormField = ({
         >
           {labelName}
         </label>
+        {isSurpriseMe && (
+          <button
+            type='button'
+            onClick={handleSurpriseMe}
+            className='font-semibold text-xs bg-[#ececf1] px-2 
+          py-1 rounded-[5px] text-black'
+          >
+            Surprise Me
+          </button>
+        )}
       </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className='bg-gray-50 border-[1px] border-gray-300
+         text-gray-900 text-sm rounded-lg focus:ring-[#4649ff]
+         focus:border-[#4649ff] outline-none block w-full p-3'
+      />
     </div>
   )
 }
